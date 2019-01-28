7dc6a23fa412f1ee949c66fe4da26fed3f0fb20b
Ian Walter <public@iankwalter.com>
2019-01-28 13:53:17 -0500

    Adding logic for macOS

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# Create the .fonts directory if necessary.
-if [ ! -d ~/.fonts ]; then
-  mkdir ~/.fonts
-fi
-
-# Copy fonts into the .fonts directory.
-cp fonts/* ~/.fonts
-
 if [[ $(uname) == 'Linux' ]]; then
+
+  # Create the .fonts directory if necessary.
+  if [ ! -d ~/.fonts ]; then
+    mkdir ~/.fonts
+  fi
+
+  # Copy fonts into the user's .fonts directory.
+  cp fonts/* ~/.fonts
 
   # Clear the fonts cache.
   sudo fc-cache -f -v
 
+fi
+
+if [[ $(uname) == 'Darwin' ]]; then
+
+  # Copy fonts into the user's fonts directory.
+  cp fonts/* ~/Library/fonts
+
+fi
+
+if [[ $? == 0 ]]; then
+  echo "Installed fonts successfully."
 fi
